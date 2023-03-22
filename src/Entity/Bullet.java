3e74933a2d93ecf89f8c94e3c273a8390430b4f7
Ama Leafee <amadeusleafmay@gmail.com>
2023-03-22 21:57:44 +0200

    addInterfaces

--- src/Entity/Bullet.java
+++ src/Entity/Bullet.java
@@ -1,56 +1,57 @@
 package Entity;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Bullet {
-    private final String type;
-    private final int damage;
+    private String type;
+    private int damage;
+    HashMap<Integer, String> types;
     public Bullet(){
-        this.type = randomType();
-        this.damage = randomDamage();
+        createTypesMap();
+        randomTypeAndDamage();
     }
-    private String randomType(){
+    private void createTypesMap(){
+        this.types = new HashMap<>();
+        types.put(0, "Regular");
+        types.put(1, "Tracing");
+        types.put(2, "Explosive");
+    }
+    private void randomTypeAndDamage(){
         int value = (int)(Math.random()*3);
-        if(value == 0){
-            return "Regular";
-        } else if (value == 1){
-            return "Tracing";
-        } else {
-            return "Explosive";
-        }
+        Function<Integer, String> randomType = x -> types.get(x);
+        Supplier<Integer> randomDamage = () -> (int)((Math.random()*(value + 1) * 10));
+        this.type = randomType.apply(value);
+        this.damage = randomDamage.get();
     }
-    private int randomDamage(){
-        if(this.type.equals("Regular")){
-            return (int)(Math.random()*10);
-        } else if (this.type.equals("Tracing")) {
-            return (int)(Math.random()*20);
-        } else {
-            return (int)(Math.random()*30);
-        }
-    }
+
     public String getType(){
         return this.type;
     }
 
-    @Override
-    public String toString() {
-        return "Type: " + this.type + "| Damage: " + this.damage;
+    public int getDamage() {
+        return damage;
     }
 
     public static List<Bullet> createMagazine(int capacity){
-        List<Bullet> magazine = Stream.generate(Bullet::new).limit(capacity).collect(Collectors.toList());
-        return magazine;
+        Supplier<Bullet> bulletGenerator = Bullet::new;
+        return Stream.generate(bulletGenerator).limit(capacity).collect(Collectors.toList());
     }
 
     public static void fire(List<Bullet> magazine){
+        Consumer<String> printer = System.out::println;
+        Consumer<Bullet> bulletPrinter = x -> System.out.println("Type: " + x.getType() + "| Damage: " + x.getDamage());
         Map<String, List<Bullet>> bulletByType = magazine.stream().collect(Collectors.groupingBy(Bullet::getType));
         bulletByType.forEach((type, magazineGrouped) -> {
-            System.out.println(type);
-            magazineGrouped.forEach((System.out::println));
-            System.out.println();
+            printer.accept(type);
+            magazineGrouped.forEach(bulletPrinter);
+            printer.accept("");
         } );
 
     }
